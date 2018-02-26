13a1c820085be9d11ec891a2ded94e77a255f3d8
P Bennett <pbennett@Ps-MacBook-Air.local>
2018-02-26 16:22:49 +0000

    created game logic after Unit 11

--- js/main.js
+++ js/main.js
@@ -32,7 +32,9 @@ if (cardsInPlay[0] === cardsInPlay[1]){
 }
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -40,9 +42,17 @@ var flipCard = function(cardId) {
 	checkForMatch();
 	
 }
-// blank comment
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement)
+}
+};
+
+createBoard();
 
 
